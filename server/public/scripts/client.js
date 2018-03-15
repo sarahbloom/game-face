f133a901466f0b9d9c620da96b113a190f65544d
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-15 13:44:41 -0500

    added info to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,27 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('document ready');
     $('#submitButton').on('click', submitGame);
+    getAllGames();// get games when page loads
+}
+
+function getAllGames(){
+    $.ajax({
+        type: 'GET',
+        url: '/game',
+    }).done(function(response){
+        appendToDom(response);//the response is the game collection array
+    });
+}
+
+function appendToDom(gameCollection){
+    $('#gameContent').empty();
+    for (let game of gameCollection){
+        console.log('game: ', game);
+        let tr = $("<tr></tr>");
+        tr.append('<td>' + game.name + '</td>' );
+        tr.append('<td>' + game.cost + '</td>');
+        $('#gameContent').append(tr);
+    }
 }
 
 function submitGame(){
@@ -15,7 +36,10 @@ function submitGame(){
         url: '/game',
     }).done(function(response) {
         //our response from POST is '200' success
+        //put the GET request in here to make sure the POST is done
         console.log('success!');  
+        //refresh game list to get new game info.
+        getAllGames();
     }).fail(function(response) {
         alert('something went wrong...');
     })
--- server/server.js
+++ server/server.js
@@ -2,14 +2,21 @@ let express = require('express');
 let app = express();
 const PORT = 4001;
 let bodyParser = require('body-parser');
-const gameCollection = [];
+const gameCollection = []; //contains objects {name: "game name", cost" $0.00}
 
 //configures bodyParser for jQuery - required
 app.use(bodyParser.urlencoded({extended:true})); 
 
 //serve static files
+//must be done before app.post!
 app.use(express.static('server/public'));
 
+//send back all the games
+app.get('/game', (req, res) => {
+    res.send(gameCollection);
+})
+
+//when we want to add a new game
 //req.body is the object we sent (req.body == gameToAdd)
 app.post('/game', (req, res) =>{
     console.log(req.body);
