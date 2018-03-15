108e63013cc613d561ffcda0273c3349f6eb2aaf
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-15 11:30:26 -0500

    add files and linked to server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+const PORT = 4001;
+
+//serve static files
+app.use(express.static('server/public'));
+
+//spin up server
+app.listen(PORT, () =>{
+    console.log('server is running on port:', PORT);
+});
