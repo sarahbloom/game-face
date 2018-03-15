4b2cc65b8da804a7d73f717f7d4e1874e17179aa
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-03-15 12:00:13 -0500

    POST request working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,21 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('document ready');
+    $('#submitButton').on('click', submitGame);
 }
+
+function submitGame(){
+    let gameName = $('#gameName').val();
+    let cost = $('#cost').val();
+    let gameToAdd = {name: gameName, cost: cost};
+    $.ajax({
+        type: 'POST',
+        data: gameToAdd,
+        url: '/game',
+    }).done(function(response) {
+        //our response from POST is '200' success
+        console.log('success!');  
+    }).fail(function(response) {
+        alert('something went wrong...');
+    })
+} 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 let express = require('express');
 let app = express();
 const PORT = 4001;
+let bodyParser = require('body-parser');
+const gameCollection = [];
+
+//configures bodyParser for jQuery - required
+app.use(bodyParser.urlencoded({extended:true})); 
 
 //serve static files
 app.use(express.static('server/public'));
 
+//req.body is the object we sent (req.body == gameToAdd)
+app.post('/game', (req, res) =>{
+    console.log(req.body);
+    let gameToAdd = req.body;
+    gameCollection.push(gameToAdd); 
+    console.log(gameCollection);
+    res.sendStatus(200);
+})
+
 //spin up server
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log('server is running on port:', PORT);
 });
